cc0ea29f35a628e3f250f9b51253e2981650d0e0
shine729 <66360301+shine729@users.noreply.github.com>
2022-08-20 03:40:43 +0900

    전체적인 스타일 수정1

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ $('.submenu>li').mouseenter(function(){
   $('.submenu>li').children('.submenu2').removeClass('on');
   $(this).children('.submenu2').addClass('on');
 });
+
+$('.slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+  ]
+});
